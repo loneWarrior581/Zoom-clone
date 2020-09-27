bd83023481b3caffacae12aa86622b17e760b359
uttkarsh <uttkarshsingh789@gmail.com>
2020-09-27 23:18:55 +0530

    added the database model

--- modals/peopleRoom.js
+++ modals/peopleRoom.js
@@ -1,3 +1,17 @@
 // saves the information regarding the room that is the name in the database for the session
-// until the connection is broken 
-const mongoose=require('mongoose');
+// until the connection is broken
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const roomSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  roomId: {
+    type: String,
+    required: true,
+  },
+});
+
+const Room = mongoose.model("user", roomSchema);
+module.exports = Room;
--- server.js
+++ server.js
@@ -1,53 +1,50 @@
 require("dotenv").config();
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
-const server=require('http') .Server(app);
-const indexRouter=require("./routes/indexRouter");
-const io=require("socket.io")(server)  // for making rooms 
-const { v4:uuidv4}=require('uuid'); //for creating the the random room id
-const {ExpressPeerServer}=require('peer');
-const peerServer=ExpressPeerServer(server,{
-    debug:true
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const server = require("http").Server(app);
+const indexRouter = require("./routes/indexRouter");
+const io = require("socket.io")(server); // for making rooms
+const { v4: uuidv4 } = require("uuid"); //for creating the the random room id
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
 });
-app.use(express.static('public'));
-app.set('view engine','pug')
-app.use('/peerjs',peerServer);
+app.use(express.static("public"));
+app.set("view engine", "pug");
+app.use("/peerjs", peerServer);
 
-mongoose.connect(process.env.MONGOURI,
-    {
-        dbName:"users",//giving the database name 
-        user:process.env.USER,
-        pass:process.env.PASS,
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-        useFindAndModify:false
-    })
-    .then(()=>{
-        console.log("Mongodb connected...");
-    })
-    .catch(()=>{
-        console.log("Connection failed...")
-    });
+mongoose
+  .connect(process.env.MONGOURI, {
+    dbName: "users", //giving the database name
+    user: process.env.USER,
+    pass: process.env.PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Mongodb connected...");
+  })
+  .catch(() => {
+    console.log("Connection failed...");
+  });
 
-app.use("/",indexRouter);
+app.use("/", indexRouter);
 
 // addind a mongoose connection
 
-io.on('connection',socket=>{
-    socket.on('join-room',(roomId,userId)=>{   //this one listnes for the client and if any one joins the room a message is printerd in the console using the the socket.emit('join-room') on the client side
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit("user-connected",userId)
-        socket.on('message',(message)=>{
-            io.to(roomId).emit('createMessage',message)
-        })
-    })
-})
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    //this one listnes for the client and if any one joins the room a message is printerd in the console using the the socket.emit('join-room') on the client side
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+  });
+});
 
-
-
-
-
-server.listen(process.env.PORT || 80,()=>{
-    console.log('the server started listning on the port 80')
+server.listen(process.env.PORT || 80, () => {
+  console.log("the server started listning on the port 80");
 });
